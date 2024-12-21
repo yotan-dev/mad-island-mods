0f1f451bcc271495311786cfab9e3ec4d7203de6
Yotan <therandom2@protonmail.com>
2024-12-21 16:48:05 -0300

    [YotanModCore] Add SexInfoIndex constants

---
+++ YotanModCore/src/Consts/SexInfoIndex.cs
@@ -0,0 +1,12 @@
+namespace YotanModCore.Consts
+{
+	public static class SexInfoIndex
+	{
+		public const int SexCount = 0;
+		public const int FirstSex = 1;
+		public const int Masturbation = 2;
+		public const int Pregnancies = 3;
+
+		public const int Deliveries = 4;
+	}
+}
