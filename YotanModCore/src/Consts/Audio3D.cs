cb71f50b00cc175cfcd6db750cfafe82439f8e63
Yotan <therandom2@protonmail.com>
2024-12-22 02:30:39 -0300

    [YotanModCore] add audio tracks const

---
+++ YotanModCore/src/Consts/Audio3D.cs
@@ -0,0 +1,12 @@
+namespace YotanModCore.Consts
+{
+	/// <summary>
+	/// Audio tracks used with Managers.mn.sound.LoopAudio3D
+	/// </summary>
+	public static class AudioTrack
+	{
+		public const int Pee1 = 91;
+		
+		public const int Pee2 = 92;
+	}
+}
