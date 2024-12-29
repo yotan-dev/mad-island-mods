46b6b9c0f2c0744251314ec6f6aea0fe902f809e
Yotan <therandom2@protonmail.com>
2024-12-29 17:58:50 -0300

    [YMC] Add meta prop to menu item

--- YotanModCore/src/PropPanels/MenuItem.cs
+++ YotanModCore/src/PropPanels/MenuItem.cs
@@ -18,14 +18,21 @@ namespace YotanModCore.PropPanels
 		public Action Action;
 
 		/// <summary>
+		/// Metadata for this menu item
+		/// </summary>
+		public int Meta;
+
+		/// <summary>
 		/// Creates a menu item with our own text
 		/// </summary>
 		/// <param name="text">Text</param>
 		/// <param name="action">Action ran when this menu item is clicked</param>
-		public MenuItem(string text, Action action)
+		/// <param name="meta">Metadata for this menu item</param>
+		public MenuItem(string text, Action action, int meta = 0)
 		{
 			Text = text;
 			Action = action;
+			Meta = meta;
 		}
 
 		/// <summary>
@@ -33,10 +40,12 @@ namespace YotanModCore.PropPanels
 		/// </summary>
 		/// <param name="textId">See PropPanelConst.Text</param>
 		/// <param name="action">Action ran when this menu item is clicked</param>
-		public MenuItem(int textId, Action action)
+		/// <param name="meta">Metadata for this menu item</param>
+		public MenuItem(int textId, Action action, int meta = 0)
 		{
 			Text = Managers.mn.textMN.actButtonTexts[textId];
 			Action = action;
+			Meta = meta;
 		}
 
 	}
