c19e6185b356713a5c6031647f3f62efbcff116d
Yotan <therandom2@protonmail.com>
2024-12-29 20:20:35 -0300

    [HF] re-enable scene patchers

--- HFramework/Plugin.cs
+++ HFramework/Plugin.cs
@@ -20,17 +20,17 @@ namespace HFramework
 
 			if (HFramework.Config.Instance.ReplaceOriginalScenes.Value) {
 				// @TODO: Rework these scenes.
-				// Harmony.CreateAndPatchAll(typeof(SexManager_AssWallPatch));
+				Harmony.CreateAndPatchAll(typeof(SexManager_AssWallPatch));
 				Harmony.CreateAndPatchAll(typeof(SexManager_CommonSexNPCPatch));
 				Harmony.CreateAndPatchAll(typeof(SexManager_CommonSexPlayerPatch));
-				// Harmony.CreateAndPatchAll(typeof(SexManager_DarumaPatch));
-				// Harmony.CreateAndPatchAll(typeof(SexManager_DeliveryPatch));
-				// Harmony.CreateAndPatchAll(typeof(SexManager_ManRapesPatch));
+				Harmony.CreateAndPatchAll(typeof(SexManager_DarumaPatch));
+				Harmony.CreateAndPatchAll(typeof(SexManager_DeliveryPatch));
+				Harmony.CreateAndPatchAll(typeof(SexManager_ManRapesPatch));
 				Harmony.CreateAndPatchAll(typeof(SexManager_ManRapesSleepPatch));
-				// Harmony.CreateAndPatchAll(typeof(SexManager_OnaniNPCPatch));
+				Harmony.CreateAndPatchAll(typeof(SexManager_OnaniNPCPatch));
 				Harmony.CreateAndPatchAll(typeof(SexManager_PlayerRapedPatch));
-				// Harmony.CreateAndPatchAll(typeof(SexManager_SlavePatch));
-				// Harmony.CreateAndPatchAll(typeof(SexManager_ToiletPatch));
+				Harmony.CreateAndPatchAll(typeof(SexManager_SlavePatch));
+				Harmony.CreateAndPatchAll(typeof(SexManager_ToiletPatch));
 
 				Harmony.CreateAndPatchAll(typeof(SexChecksPatch));
 
