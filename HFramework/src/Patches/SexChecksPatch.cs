86d29b1d32d640eccce97ced3b7ae7640f3beb44
Yotan <therandom2@protonmail.com>
2024-12-31 19:32:04 -0300

    [HF] Initial patching of RapesCheck - for now supporting PlayerRaped and ManRapesSleep

--- HFramework/src/Patches/SexChecksPatch.cs
+++ HFramework/src/Patches/SexChecksPatch.cs
@@ -32,5 +32,49 @@ namespace HFramework.Patches
 			__result = sceneInfo?.CanStart(PerformerScope.Sex, realFrom, realTo) ?? false;
 			return false;
 		}
+
+		[HarmonyPatch(typeof(SexManager), nameof(SexManager.RapesCheck))]
+		[HarmonyPrefix]
+		private static bool Pre_SexManager_RapesCheck(CommonStates from, CommonStates to, ref bool __result)
+		{
+			__result = false;
+
+			// NOTE: We don't sort actors for rape scenes, because we need to know WHO is raping and WHO is being raped.
+			//       For example, LargeNativeFemale can rape Man (thus [LargeNativeFemale, Man]),
+			//       but Man can rape LargeNativeFemale too (thus [Man, LargeNativeFemale]).
+			//       In those cases, we will have additional performers to support that.
+
+			int activePlayer = CommonUtils.GetActivePlayer().npcID;
+			SceneInfo? sceneInfo;
+			if (to.npcID == activePlayer)
+			{
+				sceneInfo = ScenesManager.Instance.GetSceneInfo(PlayerRaped.Name);
+			}
+			else if (from.npcID == activePlayer)
+			{
+				if (to.nMove.actType == NPCMove.ActType.Sleep && to.anim.state.GetCurrent(0).Animation.Name == "A_sleep")
+				{
+					sceneInfo = ScenesManager.Instance.GetSceneInfo(ManRapesSleep.Name);
+				}
+				else
+				{
+					// ManRapes
+					return true; // @TODO: We don't have this yet
+				}
+			}
+			else
+			{
+				// Npc x Npc
+				return true; // @TODO: We don't have this yet
+			}
+
+			if (sceneInfo != null)
+			{
+				__result = sceneInfo.CanStart(PerformerScope.Sex, from, to);
+				return false;
+			}
+
+			return true;
+		}
 	}
 }
