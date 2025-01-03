0012afa5475c9d9de1ccde777f3bcdcf36d40044
Yotan <therandom2@protonmail.com>
2025-01-02 21:53:49 -0300

    [HF] finish conversion of player raped to hooks

--- HFramework/src/Patches/SexManager_PlayerRapedPatch.cs
+++ HFramework/src/Patches/SexManager_PlayerRapedPatch.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using HFramework.Scenes;
 using HarmonyLib;
-using YotanModCore;
 
 namespace HFramework.Patches
 {
@@ -15,7 +12,6 @@ namespace HFramework.Patches
 		{
 			var scene = new PlayerRaped(to, from);
 			scene.Init(new DefaultSceneController());
-			scene.AddEventHandler(new DefaultSceneEventHandler());
 			__result = scene.Run();
 			return false;
 		}
