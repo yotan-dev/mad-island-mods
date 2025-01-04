7584267202853a5c7562af3d4dcbdb1247ba1c5e
Yotan <therandom2@protonmail.com>
2025-01-03 21:41:52 -0300

    [Gallery] convert delivery

---
+++ Gallery/src/GalleryScenes/Delivery/DeliveryTracker.cs
@@ -0,0 +1,22 @@
+using Gallery.SaveFile.Containers;
+
+namespace Gallery.GalleryScenes.Delivery
+{
+	public class DeliveryTracker : BaseTracker
+	{
+		private readonly GalleryChara Mother;
+
+		public DeliveryTracker(CommonStates mother) : base()
+		{
+			this.Mother = new GalleryChara(mother);
+		}
+
+		public override void End()
+		{
+			if (this.DidDelivery)
+				DeliverySceneManager.Instance.Unlock(this.Mother);
+			else
+				GalleryLogger.LogDebug($"DeliverySceneEventHandler: OnEnd: 'DidDelivery' not set -- event NOT unlocked for {this.Mother}");
+		}
+	}
+}
