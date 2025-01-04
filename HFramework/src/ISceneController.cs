ae092b19568af32898b217ea911ab72f97105d21
Yotan <therandom2@protonmail.com>
2025-01-04 02:02:37 -0300

    [HF] Clean up

--- HFramework/src/ISceneController.cs
+++ HFramework/src/ISceneController.cs
@@ -18,22 +18,12 @@ namespace HFramework
 		
 		IEnumerator LoopAnimation(IScene scene, SkeletonAnimation tmpSexAnim, string name);
 
-		IEnumerable PlayTimedStep(IScene scene, SkeletonAnimation tmpSexAnim, string name, float time);
-		
 		IEnumerator PlayTimedStep(string name, float time);
-		
-		IEnumerable PlayOnceStep(IScene scene, SkeletonAnimation tmpSexAnim, string name, bool skippable = false);
 		
 		IEnumerator PlayOnceStep(string name, bool skippable = false);
 		
 		void PlayOnceStepBg(string name);
 
-		IEnumerator PlayUntilInputStep(IScene scene, SkeletonAnimation tmpSexAnim, string name);
-
 		IEnumerator WaitForInput();
-
-		IEnumerable PlayPlayerGrappledStep(IScene scene, SkeletonAnimation tmpSexAnim, string name, CommonStates Player);
-
-		IEnumerable PlayPlayerGrapplesStep(IScene scene, SkeletonAnimation sexAnim, string sexType, CommonStates man, CommonStates girl);
 	}
 }
