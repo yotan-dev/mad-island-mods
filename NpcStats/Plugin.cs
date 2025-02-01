55543917a6c352ae49afcb53d848e7d7e2cc8c9d
Yotan <therandom2@protonmail.com>
2025-02-01 12:28:33 -0300

    [NS] enable nullable checks and clean up

--- NpcStats/Plugin.cs
+++ NpcStats/Plugin.cs
@@ -1,19 +1,13 @@
 ﻿using BepInEx;
 using HarmonyLib;
 using NpcStats.Patches;
-using UnityEngine;
 
 namespace NpcStats
 {
-	
 	[BepInPlugin("NpcStats", "NpcStats", "2.0.1")]
 	[BepInDependency("YotanModCore", "1.0.0")]
 	public class Plugin : BaseUnityPlugin
 	{
-		public static AssetBundle Assets;
-
-		public static ManagersScript ManagerScript;
-
 		private void Awake()
 		{
 			PLogger._Logger = Logger;
--- NpcStats/src/PLogger.cs
+++ NpcStats/src/PLogger.cs
@@ -4,36 +4,36 @@ namespace NpcStats
 
 	internal class PLogger
 	{
-		public static ManualLogSource _Logger;
+		public static ManualLogSource? _Logger;
 
 		public static void LogInfo(object data)
 		{
-			_Logger.LogInfo(data);
+			_Logger?.LogInfo(data);
 		}
 
 		public static void LogError(object data)
 		{
-			_Logger.LogError(data);
+			_Logger?.LogError(data);
 		}
 
 		public static void LogWarning(object data)
 		{
-			_Logger.LogWarning(data);
+			_Logger?.LogWarning(data);
 		}
 
 		public static void LogDebug(object data)
 		{
-			_Logger.LogDebug(data);
+			_Logger?.LogDebug(data);
 		}
 
 		public static void LogFatal(object data)
 		{
-			_Logger.LogFatal(data);
+			_Logger?.LogFatal(data);
 		}
 
 		public static void LogMessage(object data)
 		{
-			_Logger.LogMessage(data);
+			_Logger?.LogMessage(data);
 		}
 	}
 }
