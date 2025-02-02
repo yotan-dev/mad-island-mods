25877536a334196da3225f4bc78b4adb056566f9
Yotan <therandom2@protonmail.com>
2025-02-02 00:51:20 -0300

    [YUP] Add mosaic fix

--- YoUnnoficialPatches/src/PConfig.cs
+++ YoUnnoficialPatches/src/PConfig.cs
@@ -2,11 +2,13 @@ using BepInEx.Configuration;
 
 namespace YoUnnoficialPatches
 {
-	public class Config
+	public class PConfig
 	{
-		public static Config Instance { get; private set; } = new Config();
+		public static PConfig Instance { get; private set; } = new PConfig();
 
 		public ConfigEntry<bool> DontStartInvalidSex { get; private set; }
+		
+		public ConfigEntry<bool> FixMosaic { get; private set; }
 
 		internal void Init(ConfigFile conf)
 		{
@@ -17,6 +19,13 @@ namespace YoUnnoficialPatches
 				"Whether NPCs should try to sex when there are no scenes for them.\n"
 				+ "If true, NPCs will not try to have sex when there are no scenes for them.\n"
 				+ "If false, the original behavior will be used. 2 NPCs may try to have sex but fail once they reach the bed due to no scene being available (mod author thinks it is a bug).\n"
+			);
+
+			this.FixMosaic = conf.Bind<bool>(
+				"General",
+				"FixMosaic",
+				true,
+				"Tries to fix some censor/mosaic that the original game can't clean up properly. If true, it will fix them according to your censor configs."
 			);
 		}
 	}
