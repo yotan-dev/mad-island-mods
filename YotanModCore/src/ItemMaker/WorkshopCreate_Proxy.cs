e2c5a91deb583eebcf919b1c7b05a254486a4f6c
Yotan <therandom2@protonmail.com>
2025-06-21 10:54:55 -0300

    [YMC] Commit currently unused Item Maker classes, before we commit in another place

---
+++ YotanModCore/src/ItemMaker/WorkshopCreate_Proxy.cs
@@ -0,0 +1,8 @@
+namespace YotanModCore.ItemMaker
+{
+
+	internal class WorkshopCreate_Proxy : SteamWorkshopCreate
+	{
+		/* Nothing to do here */
+	}
+}
---
+++ YotanModCore/src/ItemMaker/WorkshopTestMove2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YotanModCore.ItemMaker
+{
+
+	internal class WorkshopTestMove2 : WorkshopTestMove
+	{
+	}
+}
