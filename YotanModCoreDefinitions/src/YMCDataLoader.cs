9b04440335ce0e92558e6c7884e901537c1093dd
Yotan <therandom2@protonmail.com>
2025-06-21 12:19:41 -0300

    [YMC] more stuff for custom items

--- YotanModCoreDefinitions/src/YMCDataLoader.cs
+++ YotanModCoreDefinitions/src/YMCDataLoader.cs
@@ -8,6 +8,7 @@ namespace YotanModCore
 	[CreateAssetMenu(fileName = "YMCDataLoader", menuName = "Yotan Mod Core/Data Loader", order = 1)]
 	public class YMCDataLoader : ScriptableObject
 	{
+		[Tooltip("Add your custom items 'inventory data' here so they get loaded into the game")]
 		public List<CustomItemData> Items = [];
 	}
 }
