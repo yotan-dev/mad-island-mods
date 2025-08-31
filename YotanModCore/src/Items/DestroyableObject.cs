a905e10fdd37ea70e83b6425287f92f326aad6e4
Yotan <therandom2@protonmail.com>
2025-08-31 17:44:35 -0300

    [YMC] Deprecate DestroyableObject

--- YotanModCore/src/Items/DestroyableObject.cs
+++ YotanModCore/src/Items/DestroyableObject.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace YotanModCore.Items
 {
 	/// <summary>
 	/// Represents an world object that can be destroyed (e.g. by Raids)
 	/// </summary>
+	[Obsolete("Use CDefenceInfo instead.")]
 	public class DestroyableObject : DefenceInfo
 	{
 		/* Proxy for DefenceInfo */
