e160b0534f5ac5c7091a19093b58ca57bf7fa451
Yotan <therandom2@protonmail.com>
2025-10-25 17:24:57 -0300

    update generator with support to provide assets folder path

--- scripts/src/generator.js
+++ scripts/src/generator.js
@@ -6,7 +6,13 @@ import { TagExtractor } from "./extractors/TagExtractor.js";
 import { LayerExtractor } from "./extractors/LayerExtractor.js";
 import { TextColorExtractor } from "./extractors/TextColorExtractor.js";
 
-const proj = new Project('../../Assets_v044/');
+const path = process.argv[2];
+if (!path) {
+	console.error('Please provide a path to the Assets folder. Usage: npm run generate ../Path/to/Assets/folder/');
+	process.exit(1);
+}
+
+const proj = new Project(path);
 await new NpcIdExtractor().extract(proj);
 await new FXExtractor().extract(proj);
 await new SoundExtractor().extract(proj);
