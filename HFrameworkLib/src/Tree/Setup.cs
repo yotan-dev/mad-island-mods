73179e3125bcfda013ff1746cfad7d74412e96ae
Yotan <therandom2@protonmail.com>
2026-02-14 13:00:25 -0300

    move offset and npc reaction as configs

--- HFrameworkLib/src/Tree/Setup.cs
+++ HFrameworkLib/src/Tree/Setup.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
+using YotanModCore;
 
 namespace HFramework.Tree
 {
 	public class Setup : ActionNode
 	{
 		public PrefabConfig prefabConfig;
+
+		// Note: When false, NPC will change out of WAIT and fall down from the world if the sex doesn't stop.
+		// CommonSexNpc -> false
+		// CommonSexPlayer -> true
+		public bool stopNpcReaction = true;
+
+		// CommonSexNPc -> new Vector3(0.0f, 0.0f, 0.02f)
+		// commonSexPlayer -> zero
+		public Vector3 posOffset = Vector3.zero;
 
 		protected override void OnStart()
 		{
@@ -29,12 +39,17 @@ namespace HFramework.Tree
 			}
 
 			// Pos offset only in CommonSexNpc
-			prefab.transform.position += new Vector3(0.0f, 0.0f, 0.02f);
+			// CommonSexNpc -> new Vector3(0.0f, 0.0f, 0.02f)
+			prefab.transform.position += this.posOffset;
+			var currentPlayer = CommonUtils.GetActivePlayer();
 			foreach (var npc in this.context.Actors)
 			{
 				npc.Angle = npc.Common.nMove.searchAngle;
-				npc.Common.nMove.searchAngle = 0f; //180f;
-				npc.Common.gameObject.transform.position = this.context.SexPlacePos.Value;
+				if (npc.Common != currentPlayer)
+				{
+					npc.Common.nMove.searchAngle = this.stopNpcReaction ? 0f : 180f;
+					npc.Common.gameObject.transform.position = this.context.SexPlacePos.Value;
+				}
 			}
 
 			this.prefabConfig.SetAppearance(prefab, this.context);
