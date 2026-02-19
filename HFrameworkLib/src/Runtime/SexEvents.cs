c3d7df2c99d92826dada50c82365a75d51c70f39
Yotan <therandom2@protonmail.com>
2026-02-19 08:33:40 -0300

    support event args type change in editor

--- HFrameworkLib/src/Runtime/SexEvents.cs
+++ HFrameworkLib/src/Runtime/SexEvents.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
 
 namespace HFramework
 {
@@ -15,52 +20,91 @@ namespace HFramework
 		}
 	}
 
+	public class SexEventInfo
+	{
+		public ISexEventBase Event { get; private set; }
+		public Type EventType { get; private set; }
+
+		public void SetEvent<T>(ISexEvent<T> evt) where T : BaseSexEventArgs, new()
+		{
+			Event = evt;
+			EventType = typeof(T);
+		}
+	}
+
 	public static class SexEvents
 	{
 		// General performance (e.g. starting to do something)
 		[SexEvent("HF", "Perform: Scissoring")]
-		public static readonly SexEvent OnPerformScissor = new SexEvent("HF.perform.scissoring");
+		public static readonly SexEvent<FromToEventArgs> OnPerformScissor = new SexEvent<FromToEventArgs>("HF.perform.scissoring");
 		[SexEvent("HF", "Perform: Titfuck")]
-		public static readonly SexEvent OnPerformTitFuck = new SexEvent("HF.perform.titfuck");
+		public static readonly SexEvent<FromToEventArgs> OnPerformTitFuck = new SexEvent<FromToEventArgs>("HF.perform.titfuck");
 		[SexEvent("HF", "Perform: Handjob")]
-		public static readonly SexEvent OnPerformHandJob = new SexEvent("HF.perform.handjob");
+		public static readonly SexEvent<FromToEventArgs> OnPerformHandJob = new SexEvent<FromToEventArgs>("HF.perform.handjob");
 		[SexEvent("HF", "Perform: Masturbation")]
-		public static readonly SexEvent OnPerformMasturbation = new SexEvent("HF.perform.masturbation");
+		public static readonly SexEvent<SelfEventArgs> OnPerformMasturbation = new SexEvent<SelfEventArgs>("HF.perform.masturbation");
 		[SexEvent("HF", "Perform: Delivery")]
-		public static readonly SexEvent OnPerformDelivery = new SexEvent("HF.perform.delivery");
+		public static readonly SexEvent<FromToEventArgs> OnPerformDelivery = new SexEvent<FromToEventArgs>("HF.perform.delivery");
 
 		// Penetration events (dick being inserted somewhere)
 		[SexEvent("HF", "Penetrate: Vagina")]
-		public static readonly SexEvent OnPenetrateVagina = new SexEvent("HF.penetrate.vagina");
+		public static readonly SexEvent<FromToEventArgs> OnPenetrateVagina = new SexEvent<FromToEventArgs>("HF.penetrate.vagina");
 		[SexEvent("HF", "Penetrate: Ass")]
-		public static readonly SexEvent OnPenetrateAss = new SexEvent("HF.penetrate.ass");
+		public static readonly SexEvent<FromToEventArgs> OnPenetrateAss = new SexEvent<FromToEventArgs>("HF.penetrate.ass");
 		[SexEvent("HF", "Penetrate: Mouth")]
-		public static readonly SexEvent OnPenetrateMouth = new SexEvent("HF.penetrate.mouth");
+		public static readonly SexEvent<FromToEventArgs> OnPenetrateMouth = new SexEvent<FromToEventArgs>("HF.penetrate.mouth");
 
 		// Lick events (tongue being used)
 		[SexEvent("HF", "Lick: Vagina")]
-		public static readonly SexEvent OnLickVagina = new SexEvent("HF.lick.vagina");
+		public static readonly SexEvent<FromToEventArgs> OnLickVagina = new SexEvent<FromToEventArgs>("HF.lick.vagina");
 
 		// Sex "finishings"
 		[SexEvent("HF", "Orgasm")]
-		public static readonly SexEvent OnOrgasm = new SexEvent("HF.orgasm");
+		public static readonly SexEvent<FromToEventArgs> OnOrgasm = new SexEvent<FromToEventArgs>("HF.orgasm");
 		[SexEvent("HF", "Cum: Vagina")]
-		public static readonly SexEvent OnCumOnVagina = new SexEvent("HF.cum.vagina");
+		public static readonly SexEvent<FromToEventArgs> OnCumOnVagina = new SexEvent<FromToEventArgs>("HF.cum.vagina");
 		[SexEvent("HF", "Cum: Ass")]
-		public static readonly SexEvent OnCumOnAss = new SexEvent("HF.cum.ass");
+		public static readonly SexEvent<FromToEventArgs> OnCumOnAss = new SexEvent<FromToEventArgs>("HF.cum.ass");
 		[SexEvent("HF", "Cum: Mouth")]
-		public static readonly SexEvent OnCumOnMouth = new SexEvent("HF.cum.mouth");
+		public static readonly SexEvent<FromToEventArgs> OnCumOnMouth = new SexEvent<FromToEventArgs>("HF.cum.mouth");
 		[SexEvent("HF", "Cum: Tits")]
-		public static readonly SexEvent OnCumOnTits = new SexEvent("HF.cum.tits");
+		public static readonly SexEvent<FromToEventArgs> OnCumOnTits = new SexEvent<FromToEventArgs>("HF.cum.tits");
 
 		// Delivery outcomes
 		[SexEvent("HF", "Delivery: Birth")]
-		public static readonly SexEvent OnGiveBirth = new SexEvent("HF.delivery.birth");
+		public static readonly SexEvent<FromToEventArgs> OnGiveBirth = new SexEvent<FromToEventArgs>("HF.delivery.birth");
 		[SexEvent("HF", "Delivery: Stillbirth")]
-		public static readonly SexEvent OnStillbirth = new SexEvent("HF.delivery.stillbirth");
+		public static readonly SexEvent<FromToEventArgs> OnStillbirth = new SexEvent<FromToEventArgs>("HF.delivery.stillbirth");
 
 		// Other
 		[SexEvent("HF", "Other: Player Defeated")]
-		public static readonly SexEvent OnPlayerDefeated = new SexEvent("HF.other.playerDefeated");
+		public static readonly SexEvent<FromToEventArgs> OnPlayerDefeated = new SexEvent<FromToEventArgs>("HF.other.playerDefeated");
+
+		public static readonly Dictionary<string, SexEventInfo> Events = new Dictionary<string, SexEventInfo>();
+
+		static SexEvents()
+		{
+			Events = new Dictionary<string, SexEventInfo>();
+
+			var fields = TypeCache.GetFieldsWithAttribute<SexEventAttribute>();
+			foreach (var fld in fields)
+			{
+				var evt = fld.GetValue(null);
+				var eventType = fld.FieldType;
+				var eventInstance = evt as ISexEventBase;
+
+				if (eventInstance != null)
+				{
+					var eventInfo = new SexEventInfo();
+					var genericType = eventType.GetGenericArguments()[0];
+					var setMethod = typeof(SexEventInfo).GetMethod("SetEvent")
+						.MakeGenericMethod(genericType);
+					setMethod.Invoke(eventInfo, new[] { evt });
+
+					Events[eventInstance.GetId()] = eventInfo;
+				}
+			}
+
+		}
 	}
 }
