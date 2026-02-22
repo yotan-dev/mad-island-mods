41d27e24e6f70709ee9059c3504a9b9c4125fa16
Yotan <therandom2@protonmail.com>
2026-02-21 21:23:06 -0300

    remove first attempt

--- HFramework/src/SexScripts/CommonSexNpcStates/SexFinishIdle.cs
+++
@@ -1,8 +0,0 @@
-namespace HFramework.SexScripts.CommonSexNpcStates
-{
-	[Experimental]
-	public class SexFinishIdle : LoopAnimForTime
-	{
-		public SexFinishIdle(BaseState onSuccessState) : base("A_Finish_idle", 8f, onSuccessState) { }
-	}
-}
--- HFramework/src/SexScripts/CommonSexNpcStates/SexSpeed1.cs
+++
@@ -1,8 +0,0 @@
-namespace HFramework.SexScripts.CommonSexNpcStates
-{
-	[Experimental]
-	public class SexSpeed1 : LoopAnimForTime
-	{
-		public SexSpeed1(BaseState onSuccessState) : base("A_Loop_01", 20f, onSuccessState) { }
-	}
-}
