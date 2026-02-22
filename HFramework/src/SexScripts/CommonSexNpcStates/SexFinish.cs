359fdd379801982fa7ea983b3073c6168bff6575
Yotan <therandom2@protonmail.com>
2026-02-21 21:23:06 -0300

    remove first attempt

--- HFramework/src/SexScripts/CommonSexNpcStates/SexFinish.cs
+++
@@ -1,9 +0,0 @@
-namespace HFramework.SexScripts.CommonSexNpcStates
-{
-	[Experimental]
-	public class SexFinish : AnimOnce
-	{
-		// @TODO: Emit events (Orgasm, creampie)
-		public SexFinish(BaseState onSuccessState) : base("A_Finish", onSuccessState) { }
-	}
-}
--- HFramework/src/SexScripts/CommonSexNpcStates/SexSpeed2.cs
+++
@@ -1,8 +0,0 @@
-namespace HFramework.SexScripts.CommonSexNpcStates
-{
-	[Experimental]
-	public class SexSpeed2 : LoopAnimForTime
-	{
-		public SexSpeed2(BaseState onSuccessState) : base("A_Loop_02", 10f, onSuccessState) { }
-	}
-}
