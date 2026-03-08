26d66f2da0fb2d9905118b01d35afe8442d579a1
Yotan <therandom2@protonmail.com>
2026-03-07 21:56:56 -0300

    [YUP] Adds 'Fix Debug Tool Input' patch. Disabled by default

--- YoUnnoficialPatches/src/PConfig.cs
+++ YoUnnoficialPatches/src/PConfig.cs
@@ -7,11 +7,12 @@ namespace YoUnnoficialPatches
 		public static PConfig Instance { get; private set; } = new PConfig();
 
 		public ConfigEntry<bool> DontStartInvalidSex { get; private set; }
-		
+
+		public ConfigEntry<bool> FixDebugToolInput { get; private set; }
+
 		public ConfigEntry<bool> FixMosaic { get; private set; }
 
-		internal void Init(ConfigFile conf)
-		{
+		internal void Init(ConfigFile conf) {
 			this.DontStartInvalidSex = conf.Bind<bool>(
 				"General",
 				"DontStartInvalidSex",
@@ -19,6 +20,18 @@ namespace YoUnnoficialPatches
 				"Whether NPCs should try to sex when there are no scenes for them.\n"
 				+ "If true, NPCs will not try to have sex when there are no scenes for them.\n"
 				+ "If false, the original behavior will be used. 2 NPCs may try to have sex but fail once they reach the bed due to no scene being available (mod author thinks it is a bug).\n"
+			);
+
+			this.FixDebugToolInput = conf.Bind<bool>(
+				"General",
+				"FixDebugToolInput",
+				false,
+				"Prevents keys from affect the game world while debug tool is open."
+				+ "For example, if you have a NPC selected and start typing a command with \"a\","
+				+ "the select NPC menu does not close because you are walking."
+				+ ""
+				+ "This is achieved by patching Unity's Input.GetKeyDown and Input.GetKey,"
+				+ "so there might be unknown side effects."
 			);
 
 			this.FixMosaic = conf.Bind<bool>(
