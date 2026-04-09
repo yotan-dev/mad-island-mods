70923118dd32fb5d08239345ea3db3c4ad763f31
Yotan <therandom2@protonmail.com>
2026-04-09 14:30:27 -0300

    Adds changelog for new changes

--- Gallery/Plugin.cs
+++ Gallery/Plugin.cs
@@ -9,7 +9,7 @@ using YotanModCore;
 
 namespace Gallery
 {
-	[BepInPlugin("Gallery", "Gallery", "1.0.12")]
+	[BepInPlugin("Gallery", "Gallery", "1.0.14")]
 	[BepInDependency("YotanModCore", "2.0.0")]
 	[BepInDependency("HFramework", "1.0.0")]
 	public class Plugin : BaseUnityPlugin
