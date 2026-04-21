87c2af015ca1a5f3a0888c5cad2d07a40c1f2bbb
Yotan <therandom2@protonmail.com>
2026-04-21 10:07:38 -0300

    [HF] Add Toggle Revive Slider node

---
+++ HFramework/src/Runtime/ScriptNodes/UI/ToggleReviveSlider.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HFramework.ScriptNodes.UI
+{
+	/// <summary>
+	/// Toggles the visibility of the revive slider UI element.
+	/// This is the "Click" input when the character is downed and struggling to get back up.
+	/// </summary>
+	[Experimental]
+	[ScriptNode("HFramework", "UI/Toggle Revive Slider")]
+	public class ToggleReviveSlider : Action
+	{
+		public bool ToVisibility;
+
+		protected override void OnStart() {
+		}
+
+		protected override State OnUpdate() {
+			GameObject.Find("UIFXPool").transform.Find("ReviveSlider").GetComponent<Slider>().gameObject.SetActive(ToVisibility);
+
+			return State.Success;
+		}
+
+		protected override void OnStop() {
+		}
+	}
+}
