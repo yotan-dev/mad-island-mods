ae4547482c5ae60434cd2bbf3296cd93ac366e3a
Yotan <therandom2@protonmail.com>
2026-04-21 10:26:55 -0300

    [HF] Make Sequence node run next node on success until it finds the first running. This save ticks between steps which is how the game originally works

--- HFramework/src/Runtime/ScriptNodes/Sequence.cs
+++ HFramework/src/Runtime/ScriptNodes/Sequence.cs
@@ -18,21 +18,34 @@ namespace HFramework.ScriptNodes
 
 		protected override State OnUpdate()
 		{
-			var child = this.children[current];
-			switch (child.Update())
-			{
-				case State.Running:
-					return State.Running;
+			// When a node successes we want the next one to be immediately executed,
+			// if we returned Running first, it would wait for the next Tick.
+			// This has a few reasons/implications:
+			// - If you have 2 nodes that needs to be executed together to prevent the game from going crazy,
+			//   this is awesome and allows it.
+			// - Not waiting the next tick makes the scripts feel more responsive (and will overall work closely to original code)
+			// - On the other hand, this might cause some unexpected node behavior -- but should be rare --
+			//   One example was the Wait node, when waiting for Input in a sequence, if we simply Succeed there
+			//   the next node would execute immediately and succeed too. In these cases, it is the node's responsibility
+			//   to put an artificial tick delay to handle this correctly.
+			while (current < this.children.Count) {
+				var child = this.children[current];
+				switch (child.Update())
+				{
+					case State.Running:
+						return State.Running;
 
-				case State.Failure:
-					return State.Failure;
+					case State.Failure:
+						return State.Failure;
 
-				case State.Success:
-					current++;
-					break;
+					case State.Success:
+						current++;
+						break;
+						/* continue to next child */
+				}
 			}
 
-			return current == children.Count ? State.Success : State.Running;
+			return State.Success;
 		}
 	}
 }
