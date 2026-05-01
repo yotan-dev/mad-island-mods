3eea31a31ab6879334948a9f7c011e30f649f016
Yotan <therandom2@protonmail.com>
2026-04-30 22:42:32 -0300

    [HF] Make LastChoiceEquals node a Sequence node child
    This should improve the DX of using it, as before you would pretty much
    always connect it to a Sequence node because you wanted to run multiple
    operations.
    
    Now, you can connect all the operations direct to it. If you have a
    single child node, it is ok, the same still works.

--- HFramework/src/Runtime/ScriptNodes/LastChoiceEquals.cs
+++ HFramework/src/Runtime/ScriptNodes/LastChoiceEquals.cs
@@ -1,8 +1,22 @@
 namespace HFramework.ScriptNodes
 {
+	/// <summary>
+	/// Node to handle menu choices. It must be a direct children of MenuInteraction node.
+	///
+	/// MenuInteraction will trigger this node if the last choice was the specified choice.
+	///
+	/// This Node will run all of its children in sequence (just like a Sequence node).
+	///
+	/// At each update cycle, the current children is called and:
+	/// - If the child is still running, it will wait for the next tick and recall the same child.
+	/// - If the child succeeds, the next one is picked immediately.
+	/// - If the child fails, the failure will be propagated to the parent node.
+	///
+	/// Once all children have succeeded, this node will succeed.
+	/// </summary>
 	[Experimental]
 	[ScriptNode("HFramework", "Menu/Last Choice Equals")]
-	public class LastChoiceEquals : Passthrough
+	public class LastChoiceEquals : Sequence
 	{
 		public string ChoiceId = "";
 
@@ -23,7 +37,7 @@ namespace HFramework.ScriptNodes
 				return State.Failure;
 			}
 
-			return this.Child != null ? this.Child.Update() : State.Success;
+			return base.OnUpdate();
 		}
 	}
 }
