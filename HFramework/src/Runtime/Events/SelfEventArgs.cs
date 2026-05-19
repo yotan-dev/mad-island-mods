3a69b2eb074df138147ff808da9fc0b8820b4bce
Yotan <therandom2@protonmail.com>
2026-05-19 19:17:18 -0300

    [HF] Make SelfEventArgs accept list of actors

--- HFramework/src/Runtime/Events/SelfEventArgs.cs
+++ HFramework/src/Runtime/Events/SelfEventArgs.cs
@@ -4,6 +4,9 @@ using HFramework.SexScripts.ScriptContext;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+// Temporary since FromNpcIdx is obsolete (Remove once we erase)
+#pragma warning disable 0618
+
 namespace HFramework.Events
 {
 	[Serializable]
@@ -12,17 +15,32 @@ namespace HFramework.Events
 	{
 		[ActorIndex]
 		[FormerlySerializedAs("fromNpcIdx")]
+		[Obsolete("Use ActorsIdx instead")]
 		public int FromNpcIdx;
+
+		[ActorIndex]
+		public int[] ActorsIdx;
+
 		[HideInInspector] public CommonStates Self;
+
+		[HideInInspector] public CommonStates[] Actors;
 
 		public override void Populate(CommonContext ctx, EmitEvent node) {
 			base.Populate(ctx, node);
 			Self = ctx.Actors[FromNpcIdx].Common;
+
+			if (ActorsIdx != null) {
+				Actors = new CommonStates[ActorsIdx.Length];
+				for (int i = 0; i < ActorsIdx.Length; i++) {
+					Actors[i] = ctx.Actors[ActorsIdx[i]].Common;
+				}
+			}
 		}
 
 		internal void CopyInspectorDataFrom(SelfEventArgs other) {
 			base.CopyInspectorDataFrom(other);
 			this.FromNpcIdx = other.FromNpcIdx;
+			this.ActorsIdx = other.ActorsIdx;
 		}
 
 		public override SexEventArgs Clone() {
@@ -33,3 +51,6 @@ namespace HFramework.Events
 		}
 	}
 }
+
+// Restore warning for other code
+#pragma warning restore 0618
